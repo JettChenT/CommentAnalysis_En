678ed959c4e24b0af3cdd53cefa507c62e9d9a23
Jettchen <jettchen12345@gmail.com>
2019-08-21 15:21:30 +0800

    Initial update, working on getting YouTube comments.

---
+++ app.py
@@ -0,0 +1,28 @@
+from sentiment import analysis
+from getcomment import getcomment
+from flask import Flask,render_template,request,abort
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/analysis',methods = ['POST'])
+def main():
+    url = request.form['url']
+    comments,status = getcomment(url)
+    if status == 1:
+        abort(404)
+    elif status == 0:
+        # print(comments)
+        return render_template('analysis.html',data=analysis(comments))
+        
+@app.route('/help')
+def help():
+    return render_template('site/index.html')
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ sentiment.py
@@ -0,0 +1,31 @@
+import nltk
+import math
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+analyzer = SentimentIntensityAnalyzer()
+def getsent(text,analyzer):
+    try:
+        score = analyzer.polarity_scores(text)
+        return score
+    except:
+        return "error"
+
+def average(list):
+    return(sum(list)/len(list))
+
+def analysis(clist):
+    # core algorithm, the exciting part
+    # improve over time
+    print(clist)
+    slist = []
+    for s in clist:
+        sen = getsent(s,analyzer)
+        if sen != 'error':
+            slist.append(sen)
+    for i in range(len(slist)):
+        slist[i] = round(slist[i],1)
+    print(slist)
+    retlist = []
+    for n in range(-10,11):
+        retlist.append(slist.count(n/10))
+    return retlist
