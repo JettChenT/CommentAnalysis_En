468ed3c2b991a725639881b1be53dc2cd5179416
Jettchen <jettchen12345@gmail.com>
2019-08-23 08:36:39 +0800

    Reformat files

--- getcomment.py
+++ getcomment.py
@@ -1,14 +1,15 @@
 """The english version of getcomment, only need to get comment from youtube
 Many bugs!!!
 """
-import re,os,json
+import re, os, json
 import googleapiclient.discovery
-from urllib.parse import urlparse,parse_qs
-with open('key','r') as f:
+from urllib.parse import urlparse, parse_qs
+
+with open("key", "r") as f:
     APIKEY = f.readline()
-with open('config.json','r') as f:
+with open("config.json", "r") as f:
     cfg = json.load(f)
-    COMMENTSMAX = cfg['commentsMax']
+    COMMENTSMAX = cfg["commentsMax"]
 # youtube api stuff
 def getCommentJson(vId):
     # Disable OAuthlib's HTTPS verification when running locally.
@@ -20,37 +21,38 @@ def getCommentJson(vId):
     DEVELOPER_KEY = APIKEY
 
     youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey = DEVELOPER_KEY)
+        api_service_name, api_version, developerKey=DEVELOPER_KEY
+    )
 
     request = youtube.commentThreads().list(
-        part="replies",
-        maxResults=COMMENTSMAX,
-        order="relevance",
-        videoId=vId
+        part="replies", maxResults=COMMENTSMAX, order="relevance", videoId=vId
     )
     response = request.execute()
     return response
+
+
 # the getcomment function
 def getVideoId(value):
     """a function to get the id of a youtube video(copied from stackoverflow)"""
-    query = urlparse('//'+value)
-    if query.netloc == 'youtu.be':
+    query = urlparse("//" + value)
+    if query.netloc == "youtu.be":
         return query.path[1:]
-    if query.netloc in ('www.youtube.com', 'youtube.com'):
-        if query.path == '/watch':
+    if query.netloc in ("www.youtube.com", "youtube.com"):
+        if query.path == "/watch":
             p = parse_qs(query.query)
-            return p['v'][0]
-        if query.path[:7] == '/embed/':
-            return query.path.split('/')[2]
-        if query.path[:3] == '/v/':
-            return query.path.split('/')[2]
+            return p["v"][0]
+        if query.path[:7] == "/embed/":
+            return query.path.split("/")[2]
+        if query.path[:3] == "/v/":
+            return query.path.split("/")[2]
     # fail?
     return None
+
 
 def getcomment(url):
     VideoId = getVideoId(url)
     if VideoId == None:
-        print('fail!!')
-        return [],0
+        print("fail!!")
+        return [], 0
     commentJson = getCommentJson(VideoId)
     print(commentJson)
--- sentiment.py
+++ sentiment.py
@@ -3,15 +3,19 @@ import math
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 analyzer = SentimentIntensityAnalyzer()
-def getsent(text,analyzer):
+
+
+def getsent(text, analyzer):
     try:
         score = analyzer.polarity_scores(text)
         return score
     except:
         return "error"
 
+
 def average(list):
-    return(sum(list)/len(list))
+    return sum(list) / len(list)
+
 
 def analysis(clist):
     # core algorithm, the exciting part
@@ -19,13 +23,13 @@ def analysis(clist):
     print(clist)
     slist = []
     for s in clist:
-        sen = getsent(s,analyzer)
-        if sen != 'error':
+        sen = getsent(s, analyzer)
+        if sen != "error":
             slist.append(sen)
     for i in range(len(slist)):
-        slist[i] = round(slist[i],1)
+        slist[i] = round(slist[i], 1)
     print(slist)
     retlist = []
-    for n in range(-10,11):
-        retlist.append(slist.count(n/10))
+    for n in range(-10, 11):
+        retlist.append(slist.count(n / 10))
     return retlist
